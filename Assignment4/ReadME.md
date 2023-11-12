e17c191d97f78fb3da00af53e0a98852ed6c37cc
Nigar Mammadova <96123286+nigarj@users.noreply.github.com>
2023-11-12 23:51:35 +0400

    Add files via upload

---
+++ Assignment4/ReadME.md
@@ -0,0 +1,12 @@
+#Features:
+Real-Time Processing: The program divides the input image into square regions and updates the display dynamically while processing it in real-time.
+Single and Multithreaded Modes: Depending on their system's capabilities, users can select between single-threaded ('S') and multi-threaded ('M') processing modes to improve performance.
+User Interaction: Using the console, the application asks the user to enter the processing mode, square size, and image file path.
+Dynamic GUI: To provide a visual representation of the changes, the graphical user interface dynamically updates to reflect the ongoing image processing.
+Image Resizing: The application resizes the original image to fit within a given width and height.
+Saving the Output: The final image is saved as "result.jpg" in the project directory once processing is finished.
+
+#How to run?
+Open the RealProcessingTime.java code file on vscode and there is a run command on the top right corner. Choose "Run Java" if you run it for the first time, otherwise choose "Run Code" option.
+
+For extra information I must explain the processes in real life (face-to-face)
---
+++ Assignment4/RealProcessingTime.java
@@ -0,0 +1,205 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class RealTimeProcessing extends JFrame {
+    private JLabel label;
+    private BufferedImage frame;
+    private int squareSize;
+
+
+    public RealTimeProcessing(String imagePath, int squareSize, char processingMode) {
+        this.squareSize = squareSize;
+
+        frame = resizeImage(ImageUtils.openImage(imagePath), 800, 1000);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        label = new JLabel(new ImageIcon(frame));
+        add(label);
+
+        pack();
+        setLocationRelativeTo(null);
+
+        if (processingMode == 'S') {
+            processImageSingleThread();
+        } else if (processingMode == 'M') {
+            processImageMultiThread();
+        } else {
+            System.out.println("Invalid processing mode. Use 'S' for single-threaded or 'M' for multi-threaded.");
+            System.exit(1);
+        }
+    }
+
+    private void processSquare(int x, int y) {
+        int left = x;
+        int upper = y;
+        int right = Math.min(x + squareSize, frame.getWidth());
+        int lower = Math.min(y + squareSize, frame.getHeight());
+
+        BufferedImage squareRegion = frame.getSubimage(left, upper, right - left, lower - upper);
+
+        int[] avgColor = averageColor(squareRegion);
+
+        Runnable updateGUI = () -> {
+            for (int sx = left; sx < right; sx++) {
+                for (int sy = upper; sy < lower; sy++) {
+                    frame.setRGB(sx, sy, new Color(avgColor[0], avgColor[1], avgColor[2]).getRGB());
+                }
+            }
+
+            label.setIcon(new ImageIcon(frame));
+            repaint();
+        };
+
+        SwingUtilities.invokeLater(updateGUI);
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int[] averageColor(BufferedImage region) {
+        int[] sum = new int[]{0, 0, 0};
+
+        for (int x = 0; x < region.getWidth(); x++) {
+            for (int y = 0; y < region.getHeight(); y++) {
+                Color pixel = new Color(region.getRGB(x, y));
+                sum[0] += pixel.getRed();
+                sum[1] += pixel.getGreen();
+                sum[2] += pixel.getBlue();
+            }
+        }
+
+        int count = region.getWidth() * region.getHeight();
+        return new int[]{sum[0] / count, sum[1] / count, sum[2] / count};
+    }
+
+    private void processImageSingleThread() {
+        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+            @Override
+            protected Void doInBackground() {
+                int[] currentPosition = new int[]{0, 0};
+
+                while (currentPosition[1] < frame.getHeight()) {
+                    processSquare(currentPosition[0], currentPosition[1]);
+                    currentPosition[0] += squareSize;
+
+                    if (currentPosition[0] >= frame.getWidth()) {
+                        currentPosition[0] = 0;
+                        currentPosition[1] += squareSize;
+                    }
+                }
+
+                saveResult();
+                return null;
+            }
+
+            @Override
+            protected void done() {
+            }
+        };
+
+        worker.execute();
+    }
+
+    private void processRow(int y) {
+        int cols = frame.getWidth() / squareSize;
+
+        SwingWorker<Void, Void> worker = new SwingWorker<>() {
+            @Override
+            protected Void doInBackground() {
+                for (int j = 0; j < cols; j++) {
+                    final int x = j * squareSize;
+                    processSquare(x, y);
+                }
+                return null;
+            }
+        };
+
+        worker.execute();
+    }
+
+    private void processImageMultiThread() {
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+
+        int rowsPerThread = frame.getHeight() / numThreads;
+
+        for (int i = 0; i < numThreads; i++) {
+            final int startY = i * rowsPerThread;
+            final int endY = (i + 1) * rowsPerThread;
+
+            executorService.submit(() -> processRowsInRange(startY, endY));
+        }
+
+        executorService.shutdown();
+        while (!executorService.isTerminated()) {
+        }
+
+        saveResult();
+    }
+
+    private void processRowsInRange(int startY, int endY) {
+        for (int y = startY; y < endY && y < frame.getHeight(); y += squareSize) {
+            processRow(y);
+        }
+    }
+
+    private void saveResult() {
+        try {
+            BufferedImage resultImage = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = resultImage.createGraphics();
+            g.drawImage(frame, 0, 0, null);
+            g.dispose();
+
+            ImageIO.write(resultImage, "jpg", new File("result.jpg"));
+            System.out.println("Result saved to: result.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
+        Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.drawImage(resultingImage, 0, 0, null);
+        g2d.dispose();
+        return resizedImage;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter image file path, square size, and processing mode (S for single-threaded, M for multi-threaded):");
+        String[] input = scanner.nextLine().split(" ");
+
+        String imagePath = input[0];
+        int squareSize = Integer.parseInt(input[1]);
+        char processingMode = input[2].toUpperCase().charAt(0);
+
+        SwingUtilities.invokeLater(() -> {
+            RealTimeProcessing realTimeProcessing = new RealTimeProcessing(imagePath, squareSize, processingMode);
+            realTimeProcessing.setVisible(true);
+        });
+    }
+}
+
+class ImageUtils {
+    public static BufferedImage openImage(String imagePath) {
+        try {
+            return ImageIO.read(new File(imagePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
